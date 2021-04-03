e58efd3e17db6e7e12be627a5d1a8e572aa08b76
fatma ecem özer <16290117@ogrenci.ankara.edu.tr>
2021-04-03 22:29:16 +0300

    Add project files.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            //Console.WriteLine(isimler[0]);
+            //isimler[4] = "İlker";
+            //Console.WriteLine(isimler[4]); hata verir
+            //isimler = new string[5]; önceki verileri kaybeder
+
+            List<string> isimler2 = new List<string> {"Engin","Murat","Kerem","Halil"};
+            //isimler2.Add("Engin");
+            Console.WriteLine(isimler2[0]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+
+
+            
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snippet
+    class Product
+    {
+        public int Id { get; set; } //primary key kod okunurluğunu artırıyor
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+
+    }
+}
